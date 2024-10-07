ea0ce463251b76ab61edc32cdbafc78df69f7fe5
Varun Rai <112015144+varp27@users.noreply.github.com>
2024-10-07 12:29:03 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,6 +1,43 @@
 const decodeTheRing = function (s, p) {
 
-    // write your code here
+  const decodeTheRing = function (s, p) {
+    const helper = (msgIndex, keyIndex) => {
+        // If both s (message) and p (decoderKey) are exhausted, return true
+        if (msgIndex === s.length && keyIndex === p.length) {
+            return true;
+        }
+
+        // If only decoderKey (p) is exhausted but message (s) isn't, return false
+        if (keyIndex === p.length) {
+            return false;
+        }
+
+        // Handle '*' wildcard (can match 0 or more characters)
+        if (p[keyIndex] === '*') {
+            // Try skipping '*' or consuming a character
+            return helper(msgIndex, keyIndex + 1) || (msgIndex < s.length && helper(msgIndex + 1, keyIndex));
+        }
+
+        // Handle '?' wildcard (matches exactly one character)
+        if (p[keyIndex] === '?') {
+            // If there are more characters in s, move to the next character
+            return msgIndex < s.length && helper(msgIndex + 1, keyIndex + 1);
+        }
+
+        // If characters in s and p match, move both pointers forward
+        if (msgIndex < s.length && s[msgIndex] === p[keyIndex]) {
+            return helper(msgIndex + 1, keyIndex + 1);
+        }
+
+        // None of the above cases match, return false
+        return false;
+    };
+
+    return helper(0, 0);
+};
+
+module.exports = decodeTheRing;
+
 
   };
   
