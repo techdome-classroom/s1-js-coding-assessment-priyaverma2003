24bc6905bca374ff95aa7da343cc755901aa96da
Varun Rai <112015144+varp27@users.noreply.github.com>
2024-10-07 12:11:06 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,7 +2,9 @@ const getTotalIsles = function (grid) {
   let islandCount=0;
   let rows=grid.length;
   let cols=grid[0].length;
-  function dfs(r,c)
+  function dfs(r,c){
+    if (r<0 )
+  }
 
 
 
